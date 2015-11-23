db2be6844c24c47af622f062c6e36cbdff7cc5f7
Brian Hines <brian@projectweekend.net>
2015-11-22 21:07:21 -0600

    Fix numerics

--- sql/V1__Create_thpl_table.sql
+++ sql/V1__Create_thpl_table.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS thpl_data
 (
     id          SERIAL PRIMARY KEY,
     sensor      TEXT,
-    temp_f      NUMERIC(6,4),
-    temp_c      NUMERIC(6,4),
-    humidity    NUMERIC(6,4),
-    pressure    NUMERIC(6,4),
-    luminosity  NUMERIC(6,4),
+    temp_f      NUMERIC(10, 4),
+    temp_c      NUMERIC(10, 4),
+    humidity    NUMERIC(10, 4),
+    pressure    NUMERIC(10, 4),
+    luminosity  NUMERIC(10, 4),
     logged_at   TIMESTAMP WITHOUT TIME ZONE
 )
