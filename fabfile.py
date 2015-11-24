2004b265e698cbf391af73ec92c7dc840a90cfc9
Brian Hines <brian@projectweekend.net>
2015-11-23 20:34:13 -0600

    Pull out

--- fabfile.py
+++ fabfile.py
@@ -2,21 +2,6 @@ import os
 from fabric.api import env, run, local, sudo, settings
 
 
-FLYWAY_DB_URL = os.getenv('FLYWAY_DB_URL')
-assert FLYWAY_DB_URL
-
-FLYWAY_DB_USER = os.getenv('FLYWAY_DB_USER')
-assert FLYWAY_DB_USER
-
-FLYWAY_DB_PASSWORD = os.getenv('FLYWAY_DB_PASSWORD')
-assert FLYWAY_DB_PASSWORD
-
-FLYWAY_MIGRATE = "flyway -url=jdbc:{0} -user={1} -password={2} migrate".format(
-    FLYWAY_DB_URL,
-    FLYWAY_DB_USER,
-    FLYWAY_DB_PASSWORD)
-
-
 def stop_service():
     with settings(warn_only=True):
         sudo('service thpl-data-logger stop')
@@ -30,12 +15,7 @@ def docker_pull():
     run('docker pull projectweekend/thpl-data-logger')
 
 
-def migrate_db():
-    run("docker run projectweekend/thpl-data-logger {0}".format(FLYWAY_MIGRATE))
-
-
 def deploy():
     stop_service()
     docker_pull()
-    migrate_db()
     start_service()
