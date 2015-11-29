fbd38c7be5b91b103257b577c8ffe01ecd59023e
Brian Hines <brian@projectweekend.net>
2015-11-29 09:27:35 -0600

    Update picloud client

--- app/config.py
+++ app/config.py
@@ -7,3 +7,9 @@ DB_PORT_5432_TCP_ADDR = os.getenv('DB_PORT_5432_TCP_ADDR', None)
 if DATABASE_URL is None:
     assert DB_PORT_5432_TCP_ADDR
     DATABASE_URL = 'postgres://postgres@{0}:5432/postgres'.format(DB_PORT_5432_TCP_ADDR)
+
+PICLOUD_SUB_URL = os.getenv('PICLOUD_SUB_URL')
+assert PICLOUD_SUB_URL
+
+PICLOUD_API_KEY = os.getenv('PICLOUD_API_KEY')
+assert PICLOUD_API_KEY
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from picloud_client import PiCloud
+from picloud_client import SubClient
+from app.config import PICLOUD_SUB_URL, PICLOUD_API_KEY
 from app.utils.database import database_connection
 
 
 def main():
     db = database_connection()
-    picloud = PiCloud(client_name='THPL-Data-Logger')
+    picloud = SubClient(
+        url=PICLOUD_SUB_URL,
+        api_key=PICLOUD_API_KEY,
+        client_name='THPL-Data-Logger')
 
     def on_thpl_data(data):
-        print(data)
         cursor = db.cursor()
         cursor.callproc('thpl_data_insert', ['home', data])
         cursor.close()
